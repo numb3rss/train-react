7d1f3282ab6bb2177fbcef4ee07dd77a26619de7
numb3rss <julien.cordier.59000@gmail.com>
2020-10-11 19:04:44 +0200

    feat() : first exercise counter

---
+++ src/Card.Component.tsx
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+import './Card.Component.scss'
+
+class Card extends Component{
+    constructor(props: any){
+        super(props);
+    }
+    
+    render() {
+        return (
+            <div className="github-profile">
+                
+            </div>
+        )
+    }
+}
+
+export default Card;
